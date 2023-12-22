9ca3874ae0185fd396b4e8e9cc6ea87994f99216
Hrishi Punde <hrishipunde9882@gmail.com>
2023-12-22 16:43:38 +0530

    Set & Get the name

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main (String[] args){
+        RWOnly rwObject = new RWOnly();
+
+        // Trying to access the private variable directly will result in a compilation error.
+        // Uncommenting the line below will cause an error.
+        //System.out.println(rwObject.name);
+
+        rwObject.setName("Hrishi");
+
+        System.out.println("Name: "+ rwObject.getName());
+
+    }
 }
